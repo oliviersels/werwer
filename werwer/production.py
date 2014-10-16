b760210e2c8ae1fbda1f799695ca2386497bedb5
Olivier Sels <olivier.sels@gmail.com>
2014-10-16 11:42:00 +0200

    Change domain settings

--- werwer/production.py
+++ werwer/production.py
@@ -3,11 +3,11 @@ DEBUG = False
 STATIC_ROOT = 'static/'
 
 ALLOWED_HOSTS = (
-    'werwer.aetherclub.be',
+    'aetherclub.be',
 )
 
 # Oauth client config
 OAUTH2_CLIENT_SETTINGS = {
     'client_id': 'werwer-prod',
-    'oauth2_endpoint': 'http://werwer.aetherclub.be/oauth2/authorize',
+    'oauth2_endpoint': 'http://aetherclub.be/oauth2/authorize',
 }
