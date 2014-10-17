343f65cdfccf2e52943706d9c024c2d5cd66c03b
Olivier Sels <olivier.sels@gmail.com>
2014-10-17 22:30:50 +0200

    Add production settings for celery

--- werwer/production.py
+++ werwer/production.py
@@ -12,3 +12,8 @@ OAUTH2_CLIENT_SETTINGS = {
     'client_id': 'werwer-prod',
     'oauth2_endpoint': 'http://aetherclub.be/oauth2/authorize',
 }
+
+# Celery
+BROKER_URL = 'sqs://'
+BROKER_TRANSPORT_OPTIONS = {'region': 'eu-west-1'}
+CELERY_ACCEPT_CONTENT = ['json']
