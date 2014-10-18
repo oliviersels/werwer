4bd5a1e18038fd22fdf81880f7277c1d869ff281
Olivier Sels <olivier.sels@gmail.com>
2014-10-18 10:54:22 +0200

    SMTP settings for production

--- werwer/production.py
+++ werwer/production.py
@@ -1,3 +1,5 @@
+import os
+
 DEBUG = False
 
 STATIC_ROOT = 'static/'
@@ -18,3 +20,12 @@ BROKER_URL = 'sqs://'
 BROKER_TRANSPORT_OPTIONS = {'region': 'eu-west-1'}
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'email-smtp.eu-west-1.amazonaws.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('SMTP_USERNAME', '')
+EMAIL_HOST_PASSWORD = os.environ.get('SMTP_PASSWORD', '')
+DEFAULT_FROM_EMAIL = 'olivier.sels@gmail.com'
