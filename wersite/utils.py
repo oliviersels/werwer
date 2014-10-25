e9d80d3a6c621828cc124606dc6408fdf2c8c1fc
Olivier Sels <olivier.sels@gmail.com>
2014-10-25 10:41:22 +0200

    Change generate_token default to a serializable class

--- wersite/utils.py
+++ wersite/utils.py
@@ -1,8 +1,18 @@
 import random
 import string
+from django.utils.deconstruct import deconstructible
 
-def generate_token_func(length):
-    return lambda: ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(length))
+
+@deconstructible
+class GenerateToken(object):
+    def __init__(self, token_length):
+        self.token_length = token_length
+
+    def __call__(self, *args, **kwargs):
+        return ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(self.token_length))
+
+    def __eq__(self, other):
+        return self.token_length == other.token_length
 
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
