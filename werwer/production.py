259b0ec6159865c856b1203fa3e3fdab33d7a7c9
Olivier Sels <olivier.sels@gmail.com>
2015-01-03 13:37:08 +0100

    Add sentry logging

--- werwer/production.py
+++ werwer/production.py
@@ -38,3 +38,46 @@ SERVER_EMAIL = 'olivier.sels@gmail.com'
 RAVEN_CONFIG = {
     'dsn': 'http://018b8249b7744d65bf1957c45a38f562:43854855bca8425d87463f81072e77a9@sentry.aetherclub.be/2',
 }
+
+# Logging via sentry
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'WARNING',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
